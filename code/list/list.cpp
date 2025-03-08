27b475b06c17499e7ee5d67c24b2812f6fb61812
hhh675597 <huanghaohuan24@mails.ucas.ac.cn>
2025-03-08 08:43:58 +0800

    list start

---
+++ code/list/list.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+typedef int Rank;
+#define ListNodePosi(T) Listnode<T>* //列表节点的地址(position)
+
+template <typename T> struct ListNode {
+//成员
+    T data; //数值
+    ListNodePosi(T) pred, succ; //前驱，后继
+//构造函数
+    ListNode() {} //对于首尾header,tailer的构造
+    ListNode(T e, ListNodePosi(T) p = NULL, ListNodePosi(T) s = NULL)
+        :data(e), pred(p), succ(s) {} //默认构造器
+//操作接口
+    ListNodePosi(T) insertAsPred(T const& e);
+    ListNodePosi(T) insertAsSucc(T const& e);
+};
+
+
+
+int main(int, char**)
+{
+
+    return 0;
+}
