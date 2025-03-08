33ca4355bc64ef1a7708b0d2d8b3b8b282fe8ef1
hhh675597 <huanghaohuan24@mails.ucas.ac.cn>
2025-03-08 16:12:33 +0800

    Chapter3ListFinished

--- code/list/list.cpp
+++ code/list/list.cpp
@@ -1,15 +1,39 @@
-#include "listNode.h"
+#include "list.h"
 #include <iostream>
 
 using namespace std;
 
-
-
-
-
-
-
 int main(int, char**)
 {
+    int nums[5] = {1, 3, 4, 2, 5};
+    List<int> l;
+    
+
+    for (int i = 0; i < 5; i++)
+        l.insertAsLast(nums[i]);
+    
+    for (int i = 0; i < 5; i++)
+        printf("%d ", l[i]);
+    printf("\n");
+    
+    l.sort();
+    for (int i = 0; i < 5; i++)
+        printf("%d ", l[i]);
+    printf("\n");
+
+    l.insertA(l.first(), 100);
+    printf("%d\n", l[1]); //输出为100
+
+    l.reverse();
+    for (int i = 0; i < l.size(); i++)
+        printf("%d ", l[i]);
+    printf("\n");
+
+    printf("%p\n", l.find(3));
+    l.remove(l.find(3));
+    for (int i = 0; i < l.size(); i++)
+        printf("%d ", l[i]);
+    printf("\n");
+
     return 0;
 }
